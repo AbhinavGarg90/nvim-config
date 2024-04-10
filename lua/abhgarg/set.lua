1c71a79706a854ec8ff4484d5161fc0756484dd9
Abhinav Garg <ag90@illinois.edu>
2024-04-09 23:07:55 -0700

    removed swap files

--- lua/abhgarg/set.lua
+++ lua/abhgarg/set.lua
@@ -12,10 +12,8 @@ vim.opt.wrap = false
 vim.opt.wrap = false
 
 
+vim.opt.swapfile = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
